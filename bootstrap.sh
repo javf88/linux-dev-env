9aeb0d97ffcd5c6161ab00483a0038e65f979b4a
Jorge Ventura <venturasac@hotmail.com>
2025-01-13 09:53:44 +0100

    updating bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,12 +15,24 @@ result=$(hdiutil attach -nomount $ISO)
 DISK=$(echo $result | sed "s/ .*//")
 mount -t cd9660 $DISK ubuntu
 
-echo "Extracting kernel and initrd into ext"
-cp ubuntu/casper/vmlinuz vmlinux.gz
-gzip -d vmlinux.gz
-mkdir ext
-mv vmlinux ext
-cp ubuntu/casper/initrd ext
+if [ -d ext ]; then
+    echo "bootloader extracted already."
+else
+    echo "Extracting kernel and initrd into ext"
+    cp ubuntu/casper/vmlinuz vmlinux.gz
+    gzip -d vmlinux.gz
+
+    mkdir ext
+    mv vmlinux ext
+    cp ubuntu/casper/initrd ext
+fi
+
+if [ -d "ext/BlockDevice.dmg" ]; then
+    echo "BlockDevide.dmg exists already."
+else
+    echo "Creating disk image"
+    hdiutil create -size 30g -type UDIF ext/BlockDevice.dmg
+fi
 
 echo "Unmounting and cleaning the workspace"
 umount ubuntu
